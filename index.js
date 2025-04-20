b3b5c33a70de0bf5e1dc843c37a0df86ef12de13
esha-pandya0203 <pandya.e@northeastern.edu>
2025-04-20 16:55:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import session from "express-session";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.use(
+  cors({
+    credentials: true, // support cookies
+    origin: process.env.NETLIFY_URL || "http://localhost:5173", // restrict cross origin resource sharing to react application
+  })
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT || 4000);
